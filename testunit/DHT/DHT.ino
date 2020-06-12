8bb41fa4ba1f4d947badf740d0338a7df900ae15
PearAreeya <61303454+PearAreeya@users.noreply.github.com>
2020-06-12 16:03:56 +0700

    Update DHT.ino

--- testunit/DHT/DHT.ino
+++ testunit/DHT/DHT.ino
@@ -6,7 +6,7 @@ void setup()
 {
   Serial.begin(9600);
   Serial.println();
-  Serial.println("Status\tHumidity (%)\tTemperature (C)\t(F)");
+  Serial.println("Status\tHumidity (%)\tTemperature (C)");
 
   dht.setup(A1); 
 }
@@ -18,10 +18,8 @@ void loop()
   float humidity = dht.getHumidity();
   float temperature = dht.getTemperature();
 
-  Serial.print(dht.getStatusString());
-  Serial.print("\t");
   Serial.print(humidity, 1);
-  Serial.print("\t\t");
+  Serial.print("\t");
   Serial.print(temperature, 1);
   Serial.print("\t\t");
   Serial.println(dht.toFahrenheit(temperature), 1);
